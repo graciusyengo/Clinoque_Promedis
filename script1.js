e77065396baca423d247052273821c7fa2084b15
gracius <yengogracius1996@gmail.com>
2024-07-20 19:16:57 +0100

    add script whattsapp

---
+++ script1.js
@@ -0,0 +1,5 @@
+function whatsappRedirect ()  {
+    const api = 'https://api.whatsapp.com/send?phone=+243982382176&text=Bonjour,  Nous somme la Clinique Promedis veuillez télecharger whatsapp pour communiquer avec nous ';
+    window.location.href = api;
+    console.log('dds,dk,')
+  };
